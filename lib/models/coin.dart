b0b4804272d78e9a0962b1a862aa647f764a06f1
ProgRS <lfernandotc@hotmail.com>
2022-08-23 20:39:08 -0300

    Cria pagina Carteira, desafio 02 Everest

---
+++ lib/models/coin.dart
@@ -0,0 +1,34 @@
+class Moeda{
+  String carteira;
+  String valorCarteira;
+  String rendimento;
+  String moeda;
+  String sigla;
+  String icone;
+  String cdi;
+  String valEthLtc;
+  String valBtc;
+  double variacao1;
+  double variacao2;
+  double variacao3;
+  bool alterarValorCarteira;
+
+
+  Moeda({
+    required this.carteira,
+    required this.valorCarteira,
+    required this.rendimento,
+    required this.moeda,
+    required this.sigla,
+    required this.icone,
+    required this.cdi,
+    required this.valEthLtc,
+    required this.valBtc,
+    required this.variacao1,
+    required this.variacao2,
+    required this.variacao3,
+    required this.alterarValorCarteira
+
+
+  });
+}
