e51bd2e7e45c156cd2b4f8be7d4cfaa594b42ef7
ProgRS <lfernandotc@hotmail.com>
2022-09-08 10:49:50 -0300

    Refatoracao

--- lib/pages/carteiraApp.dart
+++ lib/pages/carteiraApp.dart
@@ -4,170 +4,115 @@ import 'package:flutter/material.dart';
 import '../repositories/coin_repository.dart';
 
 class TelaInicialWidget extends StatefulWidget {
-  TelaInicialWidget({Key? key}) : super(key: key);
+
+  const TelaInicialWidget({Key? key}) : super(key: key);
 
   @override
   State<TelaInicialWidget> createState() => TelaInicialState();
+
 }
 
 class TelaInicialState extends State<TelaInicialWidget> {
-  
-  final tabela = MoedaRepository.tabela;
-  bool opacidade = true;
-  String carteira = 'Carteira';
-  @override
-  Widget build(BuildContext context) {
+
+    final tabela = MoedaRepository.tabela;
+    
+    bool opacidade = true;
+    String carteira = 'Carteira';
+    
+    Widget listagemCrypto(int moeda){
+         return
+             Column(
+                children: <Widget>[
+                  Container(
+                      height: 0,
+                  ),
+                  ListTile(
+                       leading: SizedBox( width: 30, child: Image.asset(tabela[moeda].icone),),
+                       title: Text(tabela[moeda].sigla),
+                       subtitle: Text(tabela[moeda].moeda),
+                      trailing: AnimatedOpacity(
+                        opacity: opacidade ? 1 : 0,
+                        duration: const Duration(milliseconds: 800),
+                        child: Column(
+                          children: [
+                            Text(tabela[moeda].valor),
+                            Text('${tabela[moeda].variacao}%',
+                              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: (tabela[moeda].variacao > 0) ? Colors.green : Colors.red,
+                              backgroundColor: (tabela[moeda].variacao > 0) ? Colors.green[50] : Colors.red[50]
+                            ),)
+                          ],
+                        ),
+                      ),
+                   
+                  )
+                ],
+         );
+    }
+
+ 
+
+    @override
+  Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+       theme: ThemeData(
+         primarySwatch: Colors.pink,
+       ),
       home: Scaffold(
           bottomNavigationBar: BottomNavigationBar(
+
             currentIndex: 1,
-            items: [
+            items: const [
               BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.wallet_travel), label: 'Carteiras'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.tune), label: 'Movimentações')
+              BottomNavigationBarItem(icon: Icon(Icons.wallet_travel), label: 'Carteiras'),
+              BottomNavigationBarItem(icon: Icon(Icons.tune), label: 'Movimentações')
             ],
-          ),
+          ) ,
           body: ListView(
             children: <Widget>[
               Container(
                   height: 30,
-                  margin: EdgeInsets.all(10.00),
-                  padding: EdgeInsets.all(1),
+                  margin: const EdgeInsets.all(10.00),
+                  padding: const EdgeInsets.all(1),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         carteira,
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontSize: 30,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      FloatingActionButton(
-                        onPressed: () {
-                          setState(() {
-                            opacidade = !opacidade;
-                          });
-                        },
-                        child: Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.black,
-                        ),
+                      FloatingActionButton(onPressed: () {
+                        setState(() {
+                          opacidade = !opacidade;
+                        });
+                      },
                         backgroundColor: Colors.white.withOpacity(0),
+                        child: const Icon(Icons.remove_red_eye, color: Colors.black,
+                        ),
                       ),
                     ],
-                  )),
-              AnimatedOpacity(
-                opacity: opacidade ? 1 : 0,
-                duration: Duration(milliseconds: 800),
-                child: ListTile(
-                  title: Text(
-                    tabela[0].valorCarteira,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
                   ),
-                ),
               ),
-              Column(
-                children: <Widget>[                  
-                     ListTile(
-                      leading: Image.asset(
-                        tabela[0].icone,
-                        width: 30,
-                      ),
-                      title: Text(tabela[0].sigla),
-                      subtitle: Text(tabela[0].moeda),
-                      trailing: AnimatedOpacity(
-                        opacity: opacidade ? 1 : 0,
-                        duration: Duration(milliseconds: 800),
-                        child: Column(
-                          children: [
-                            Text(tabela[0].valor),
-                            Text(
-                              '+${tabela[0].variacao}%',
-                              style: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.bold,
-                                  color: (tabela[0].variacao > 0)
-                                      ? Colors.green
-                                      : Colors.red,
-                                  backgroundColor: (tabela[0].variacao > 0)
-                                      ? Colors.green[50]
-                                      : Colors.red[50]),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),                  
-                  
-                     ListTile(
-                      leading: Image.asset(
-                        tabela[1].icone,
-                        width: 30,
-                      ),
-                      title: Text(tabela[1].sigla),
-                      subtitle: Text(tabela[1].moeda),
-                      trailing: AnimatedOpacity(
-                        opacity: opacidade ? 1 : 0,
-                        duration: Duration(milliseconds: 800),
-                        child: Column(
-                          children: [
-                            Text(tabela[1].valor),
-                            Text(
-                              '+${tabela[1].variacao}%',
-                              style: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.bold,
-                                  color: (tabela[1].variacao > 0)
-                                      ? Colors.green
-                                      : Colors.red,
-                                  backgroundColor: (tabela[1].variacao > 0)
-                                      ? Colors.green[50]
-                                      : Colors.red[50]),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),                  
-                  
-                     ListTile(
-                      leading: Image.asset(
-                        tabela[2].icone,
-                        width: 30,
-                      ),
-                      title: Text(tabela[2].sigla),
-                      subtitle: Text(tabela[2].moeda),
-                      trailing: AnimatedOpacity(
-                        opacity: opacidade ? 1 : 0,
-                        duration: Duration(milliseconds: 800),
-                        child: Column(
-                          children: [
-                            Text(tabela[2].valor),
-                            Text(
-                               '${tabela[2].variacao}%' ,
-                              style: TextStyle(
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.bold,
-                                  color: (tabela[2].variacao > 0)
-                                      ? Colors.green
-                                      : Colors.red,
-                                  backgroundColor: (tabela[2].variacao > 0)
-                                      ? Colors.green[50]
-                                      : Colors.red[50]),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),                  
-                ],
-              )
+              AnimatedOpacity(
+                  opacity: opacidade ? 1 : 0,
+                  duration: const Duration(milliseconds: 800),
+              child:Container(
+                height: 30,
+                margin: const EdgeInsets.all(10.00),
+                padding: const EdgeInsets.all(1),
+                child: Text(tabela[0].valorCarteira, style: const TextStyle(fontSize: 30,
+                  fontWeight: FontWeight.bold,),),
+              )),
+              listagemCrypto(0),
+              listagemCrypto(1),
+              listagemCrypto(2),
             ],
-          )),
+          ),
+
+      )
     );
   }
 }
